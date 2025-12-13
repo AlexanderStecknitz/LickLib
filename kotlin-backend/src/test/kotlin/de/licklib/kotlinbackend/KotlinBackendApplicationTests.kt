2229f066329495de47cefc64ef757d14d0c243d8
alexanderstecknitz <Alexander@Stecknitz.eu>
2025-12-13 20:00:51 +0100

    initial: kotlin-backend

---
+++ kotlin-backend/src/test/kotlin/de/licklib/kotlinbackend/KotlinBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.licklib.kotlinbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
